fee915fb95e68983f0aa044bf8f5333eddcdf3cb
Leekyoeun <44278287+Leekyoeun@users.noreply.github.com>
2020-03-29 17:19:29 +0900

    20200329_two commit

--- app/src/main/java/com/emirim/lifecurveitshow/LoginActivity.kt
+++ app/src/main/java/com/emirim/lifecurveitshow/LoginActivity.kt
@@ -3,23 +3,20 @@ package com.emirim.lifecurveitshow
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login.*
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity()  {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
         RegisterButton.setOnClickListener {
             startActivity(Intent(this, RegisterActivity::class.java))
-        }
 
-        LoginButton.setOnClickListener {
-            startActivity(Intent(this, ChooseGrapicsActivity::class.java))
-            //DB에서 아이디랑 비밀번호 체크하는 부분
-            Toast.makeText(this,"로그인 되었습니다",Toast.LENGTH_SHORT).show()
+            LoginButton.setOnClickListener {
+                startActivity(Intent(this, ChooseGrapicsActivity::class.java))
+                //DB에서 아이디랑 비밀번호 체크하는 부분
+            }
         }
     }
 }
--- app/src/main/java/com/emirim/lifecurveitshow/MainActivity.kt
+++ app/src/main/java/com/emirim/lifecurveitshow/MainActivity.kt
@@ -3,7 +3,10 @@ package com.emirim.lifecurveitshow
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.LifeCurvelogo
 
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
         LifeCurvelogo.setOnClickListener{
             startActivity(Intent(this, LoginActivity::class.java))
