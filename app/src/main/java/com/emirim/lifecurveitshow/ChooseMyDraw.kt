04e13284ce5805547a0be59e166ec07c5d363ba4
Leekyoeun <44278287+Leekyoeun@users.noreply.github.com>
2020-04-03 08:00:38 +0900

    20200403_first commit

--- app/src/main/java/com/emirim/lifecurveitshow/ChooseMyDraw.kt
+++ app/src/main/java/com/emirim/lifecurveitshow/ChooseMyDraw.kt
@@ -1,12 +1,18 @@
 package com.emirim.lifecurveitshow
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_choose_my_draw.*
 
 class ChooseMyDraw : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_choose_my_draw)
+
+        SaveButton.setOnClickListener {
+            startActivity(Intent(this, ChooseGrapicsActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/emirim/lifecurveitshow/MainActivity.kt
+++ app/src/main/java/com/emirim/lifecurveitshow/MainActivity.kt
@@ -1,14 +1,9 @@
 package com.emirim.lifecurveitshow
 
 import android.content.Intent
-import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.auth.FirebaseAuth
-import kotlinx.android.synthetic.main.activity_login.*
+import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_main.LifeCurvelogo
-
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,8 +11,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        LifeCurvelogo.setOnClickListener{
+        LifeCurvelogo.setOnClickListener {
             startActivity(Intent(this, LoginActivity::class.java))
         }
     }
+
 }
