1cfa85397292d15973b7053f80ae63bf24a769d8
Leekyoeun <44278287+Leekyoeun@users.noreply.github.com>
2020-04-02 18:55:31 +0900

    20200402_two commit

--- app/src/main/java/com/emirim/lifecurveitshow/MainActivity.kt
+++ app/src/main/java/com/emirim/lifecurveitshow/MainActivity.kt
@@ -2,6 +2,7 @@ package com.emirim.lifecurveitshow
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
@@ -14,7 +15,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
 
         LifeCurvelogo.setOnClickListener{
             startActivity(Intent(this, LoginActivity::class.java))
