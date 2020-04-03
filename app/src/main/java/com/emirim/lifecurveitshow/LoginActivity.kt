59768d9e2ca180cf78e56a045d957c60a4af0d37
Leekyoeun <44278287+Leekyoeun@users.noreply.github.com>
2020-04-04 07:36:51 +0900

    20200404_first_commit

--- app/src/main/java/com/emirim/lifecurveitshow/LoginActivity.kt
+++ app/src/main/java/com/emirim/lifecurveitshow/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.emirim.lifecurveitshow
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity()  {
@@ -12,12 +13,15 @@ class LoginActivity : AppCompatActivity()  {
         setContentView(R.layout.activity_login)
         RegisterButton.setOnClickListener {
             startActivity(Intent(this, RegisterActivity::class.java))
-
+        }
             LoginButton.setOnClickListener {
                 startActivity(Intent(this, ChooseGrapicsActivity::class.java))
                 //DB에서 아이디랑 비밀번호 체크하는 부분
 
             }
+
+
         }
-    }
+
+
 }
--- app/src/main/java/com/emirim/lifecurveitshow/MainActivity.kt
+++ app/src/main/java/com/emirim/lifecurveitshow/MainActivity.kt
@@ -1,9 +1,17 @@
 package com.emirim.lifecurveitshow
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.jar.Manifest
+import android.os.Handler as Handler
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,5 +23,4 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this, LoginActivity::class.java))
         }
     }
-
 }
