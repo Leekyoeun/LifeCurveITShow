76b3b759b386e3494ebdca6cace98f0cf95e5754
Leekyoeun <44278287+Leekyoeun@users.noreply.github.com>
2020-04-08 18:28:44 +0900

    20200408_first_commit

--- app/src/main/java/com/emirim/lifecurveitshow/Kotlin/MainActivity.kt
+++ app/src/main/java/com/emirim/lifecurveitshow/Kotlin/MainActivity.kt
@@ -15,50 +15,70 @@ import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
 
-    private val RECORD_REQUEST_CODE=1000
-    private val TAG="PermissionDemo"
+    private val RECORD_REQUEST_CODE = 1000
+    private val TAG = "PermissionDemo"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         setupPermissions()
+
+        LifeCurvelogo.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
     }
 
-    private fun setupPermissions(){
-        //스토리지 읽기 퍼미션
-        val permission=ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+    private fun setupPermissions() {
+        //스토리지 읽기 퍼미션을 permission 변수에 담는다
+        val permission = ContextCompat.checkSelfPermission(
+            this,
+            android.Manifest.permission.READ_EXTERNAL_STORAGE
+        )
 
-        if(permission!= PackageManager.PERMISSION_GRANTED){
+        if (permission != PackageManager.PERMISSION_GRANTED) {
             Log.i(TAG, "Permission to record denied")
             makeRequest()
         }
     }
-    private fun makeRequest(){
-        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), RECORD_REQUEST_CODE)
+
+    private fun makeRequest() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+            RECORD_REQUEST_CODE
+        )
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int,
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
-        when(requestCode){
-            RECORD_REQUEST_CODE->{
-                if(grantResults.isEmpty()|| grantResults[0] != PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(this, "권한거부됨",Toast.LENGTH_SHORT).show()
-            }else{
+        when (requestCode) {
+            RECORD_REQUEST_CODE -> {
+                if (grantResults.isEmpty() || grantResults[0]  != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "권한 거부됨", Toast.LENGTH_SHORT).show()
+                } else {
                     getAllPhotos()
                 }
                 return
             }
-
         }
     }
-    private fun getAllPhotos(){
-        val cursor=getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, null, null, null, MediaStore.Images.ImageColumns.DATE_TAKEN+"DESC")
 
-        if(cursor!=null){
-            while(cursor.moveToNext()){
-                val uri=cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA))
+    private fun getAllPhotos() {
+        val cursor = getContentResolver().query(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            null,
+            null,
+            null,
+            MediaStore.Images.ImageColumns.DATE_TAKEN + " DESC"
+
+        )
+        if (cursor != null) {
+            while (cursor.moveToNext()) {
+                val uri =
+                    cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA))
                 Log.d("MainActivity", uri)
             }
             cursor.close()
