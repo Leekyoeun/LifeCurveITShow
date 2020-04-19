79636cc5352f9a5e4b4008da162623312e83a25b
Leekyoeun <44278287+Leekyoeun@users.noreply.github.com>
2020-04-19 14:08:02 +0900

    20200419_first_commit

--- app/src/main/java/com/emirim/lifecurveitshow/Kotlin/LoginActivity.kt
+++ app/src/main/java/com/emirim/lifecurveitshow/Kotlin/LoginActivity.kt
@@ -3,26 +3,23 @@ package com.emirim.lifecurveitshow.Kotlin
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.util.Patterns
-import android.view.View
-import android.widget.EditText
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.emirim.lifecurveitshow.R
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginResult
+import com.facebook.login.widget.LoginButton
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.android.gms.tasks.Task
 import com.google.android.material.snackbar.Snackbar
-import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
-import java.util.regex.Pattern
 
 
 class LoginActivity : AppCompatActivity() {
@@ -48,7 +45,7 @@ class LoginActivity : AppCompatActivity() {
         }
         googlelogin.setOnClickListener { signIn() }
 
-        //Google 로그인 옵션 ㄱ성
+        //Google 로그인 옵션 구성
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN)
             .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
@@ -133,5 +130,5 @@ class LoginActivity : AppCompatActivity() {
         googleSignInClient.revokeAccess().addOnCompleteListener(this){
 
         }
-    }
+    }//Google Login 끝
 }
