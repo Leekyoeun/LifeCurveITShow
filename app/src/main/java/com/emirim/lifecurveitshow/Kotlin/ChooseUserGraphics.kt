7b9d29310a53d133cf68c61dd2d3126b137cd85e
Leekyoeun <44278287+Leekyoeun@users.noreply.github.com>
2020-04-26 16:29:21 +0900

    20200426_commit

--- app/src/main/java/com/emirim/lifecurveitshow/Kotlin/ChooseUserGraphics.kt
+++ app/src/main/java/com/emirim/lifecurveitshow/Kotlin/ChooseUserGraphics.kt
@@ -1,13 +1,19 @@
 package com.emirim.lifecurveitshow.Kotlin
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.emirim.lifecurveitshow.R
+import kotlinx.android.synthetic.main.activity_choose_grapics.*
 
 class ChooseUserGraphics : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_choose_user_graphics)
+        setContentView(R.layout.activity_post)
+
+        chooseUserGrapics.setOnClickListener {
+            startActivity(Intent(this, PostActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/emirim/lifecurveitshow/Kotlin/GraphicPostActivity.kt
@@ -0,0 +1,13 @@
+package com.emirim.lifecurveitshow.Kotlin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.emirim.lifecurveitshow.R
+
+class GraphicPostActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_graphic_post)
+    }
+}
