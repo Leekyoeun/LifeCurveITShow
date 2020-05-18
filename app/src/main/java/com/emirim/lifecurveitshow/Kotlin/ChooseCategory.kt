3e7128e5ce7614ce30aa4eda0ad40dad482c5fe2
s2018 <s2018s35@e-mirim.hs.kr>
2020-05-18 21:07:19 +0900

    20200518

--- app/src/main/java/com/emirim/lifecurveitshow/Kotlin/ChooseCategory.kt
+++ app/src/main/java/com/emirim/lifecurveitshow/Kotlin/ChooseCategory.kt
@@ -3,6 +3,7 @@ package com.emirim.lifecurveitshow.Kotlin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.emirim.lifecurveitshow.Kotlin.Chart.MPChartActivity
 import com.emirim.lifecurveitshow.R
 import kotlinx.android.synthetic.main.activity_choose_category.*
 
@@ -12,5 +13,8 @@ class ChooseCategory : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_choose_category)
 
+        Graphic1.setOnClickListener {
+            startActivity(Intent(this, MPChartActivity::class.java))
+        }
     }
 }
