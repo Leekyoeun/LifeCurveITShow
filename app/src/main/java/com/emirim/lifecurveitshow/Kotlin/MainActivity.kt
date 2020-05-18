a355686a2794314edcdeb7bf8857724948c65b08
s2018 <s2018s35@e-mirim.hs.kr>
2020-05-18 19:54:52 +0900

    20200518

--- app/src/main/java/com/emirim/lifecurveitshow/Kotlin/MainActivity.kt
+++ app/src/main/java/com/emirim/lifecurveitshow/Kotlin/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity() {
 
         setupPermissions()
 
-        LifeCurvelogo.setOnClickListener {
+        LifeCurvelogo.setOnClickListener{
             startActivity(Intent(this, LoginActivity::class.java))
         }
     }
