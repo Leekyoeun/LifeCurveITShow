b5fb5d8e436053a32bb071a28caa1b0164a7c56b
s2018 <s2018s35@e-mirim.hs.kr>
2020-06-02 21:43:56 +0900

    20200602

--- app/src/main/java/com/emirim/lifecurveitshow/Kotlin/Login/LoginMainActivity.kt
+++ app/src/main/java/com/emirim/lifecurveitshow/Kotlin/Login/LoginMainActivity.kt
@@ -4,29 +4,45 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.emirim.lifecurveitshow.Kotlin.ChooseCategory
 import com.emirim.lifecurveitshow.Kotlin.FacebookLoginActivity
 import com.emirim.lifecurveitshow.Kotlin.GoogleLoginActivity
 import com.emirim.lifecurveitshow.Kotlin.Register.RegisterActivity
 import com.emirim.lifecurveitshow.R
 import kotlinx.android.synthetic.main.activity_login.*
+import me.aflak.libraries.FingerprintCallback
+import me.aflak.libraries.FingerprintDialog
 
 class LoginMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_login)
 
-        facebooklogin.setOnClickListener {
-            startActivity(Intent(this, FacebookLoginActivity::class.java))
-        }
-        googlelogin.setOnClickListener {
-            startActivity(Intent(this, GoogleLoginActivity::class.java))
-        }
-        RegisterButton.setOnClickListener {
-            startActivity(Intent(this, RegisterActivity::class.java))
-        }
-        LoginButton.setOnClickListener {
-            startActivity(Intent(this, LoginMainActivity::class.java))
-            Toast.makeText(applicationContext, "로그인 되었습니다", Toast.LENGTH_SHORT).show()
+            facebooklogin.setOnClickListener {
+                startActivity(Intent(this, FacebookLoginActivity::class.java))
+            }
+            googlelogin.setOnClickListener {
+                startActivity(Intent(this, GoogleLoginActivity::class.java))
+            }
+            RegisterButton.setOnClickListener {
+                startActivity(Intent(this, RegisterActivity::class.java))
+            }
+            button.setOnClickListener {
+
+           FingerprintDialog.initialize(this)
+               .title("지문인증")
+               .message("지문으로 인증합니다")
+                .callback(object: FingerprintCallback{
+                   override fun onAuthenticationSuccess() {
+                       Toast.makeText(applicationContext,"인증 성공", Toast.LENGTH_SHORT).show()
+                       //startActivity(Intent(this, ChooseCategory::class.java))
+                   }
+
+                   override fun onAuthenticationCancel() {
+                       Toast.makeText(applicationContext,"인증 실패",Toast.LENGTH_SHORT).show()
+                   }
+               })
+               .show()
         }
 
     }
