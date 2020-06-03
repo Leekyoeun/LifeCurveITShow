5e61324bc33f2dfeb0e49f9b009177d3b4f3128d
s2018 <s2018s35@e-mirim.hs.kr>
2020-06-04 08:57:18 +0900

    20200604

--- app/src/main/java/com/emirim/lifecurveitshow/Kotlin/ChooseGrapicsActivity.kt
+++ app/src/main/java/com/emirim/lifecurveitshow/Kotlin/ChooseGrapicsActivity.kt
@@ -8,13 +8,15 @@ import kotlinx.android.synthetic.main.activity_choose_grapics.*
 import org.goldenage.com.goldenage.ChartMainActivity
 
 class ChooseGrapicsActivity : AppCompatActivity() {
-    val Gallery = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_choose_grapics)
 
-
+        //카테고리 선택을 눌렀을 때
+        choosecategoryButton. setOnClickListener{
+            startActivity(Intent(this, ChooseCategory::class.java))
+        }
         //직접 그리는 것을 선택했을 때
         chooseUserGrapics.setOnClickListener {
             startActivity(Intent(this, ChartMainActivity::class.java))
